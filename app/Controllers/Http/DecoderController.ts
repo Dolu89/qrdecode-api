94b2f3c0753547dd1650bbafcdb9663c509c0a37
Dolu <doludev@protonmail.com>
2021-07-05 13:14:23 +0200

    Fix png decode

--- app/Controllers/Http/DecoderController.ts
+++ app/Controllers/Http/DecoderController.ts
@@ -3,49 +3,66 @@ import DecoderValidator from 'App/Validators/DecoderValidator'
 import jsQR from 'jsqr'
 import { PNG } from 'pngjs'
 import got from 'got'
+import FileType from 'file-type'
+import ImageData from 'App/Models/ImageData'
+import jpeg from 'jpeg-js'
 
 export default class DecodesController {
   public async index({ request, response }: HttpContextContract) {
-    try {
-      const { fileurl } = await request.validate(DecoderValidator)
+    const { fileurl } = await request.validate(DecoderValidator)
 
-      const imgResponse = await got(fileurl, { responseType: 'buffer' })
+    const imgResponse = await got(fileurl, { responseType: 'buffer' })
 
-      const imageData = await this.getImageDataFromPng(imgResponse.body)
-
-      const imageUint8 = new Uint8ClampedArray(
-        imageData.data,
-        imageData.data.byteOffset,
-        imageData.data.byteLength
-      )
-
-      var code = jsQR(imageUint8, imageData.width, imageData.height, {
-        inversionAttempts: 'dontInvert',
-      })
-
-      if (code === null) {
-        return response.unprocessableEntity('Unable to decode QR code')
-      }
-
-      return code.data
-    } catch (error) {
+    if (imgResponse.statusCode !== 200) {
       return response.unprocessableEntity('Unable to decode QR code')
     }
+
+    const fileType = await FileType.fromBuffer(imgResponse.body)
+
+    if (!fileType?.mime) {
+      return response.unprocessableEntity('Unable to decode QR code')
+    }
+
+    const imageData = await this.getImageDataFromBuffer(imgResponse.body, fileType?.mime)
+
+    if (!imageData?.data) {
+      return response.unprocessableEntity('Unable to decode QR code')
+    }
+
+    const imageUint8 = new Uint8ClampedArray(
+      imageData.data,
+      imageData.data.byteOffset,
+      imageData.data.byteLength
+    )
+
+    var code = jsQR(imageUint8, imageData.width, imageData.height, {
+      inversionAttempts: 'dontInvert',
+    })
+
+    if (code === null) {
+      return response.unprocessableEntity('Unable to decode QR code')
+    }
+
+    return code.data
   }
 
-  public async getImageDataFromPng(
-    fileBuffer: ArrayBuffer
-  ): Promise<{ data: Buffer; width: number; height: number }> {
+  public async getImageDataFromBuffer(fileBuffer: ArrayBuffer, mime: string): Promise<ImageData> {
     return new Promise(async (resolve, reject) => {
-      let png = new PNG({ filterType: 4 })
+      if (mime === 'image/jpeg') {
+        resolve(jpeg.decode(fileBuffer))
+      } else if (mime === 'image/png') {
+        let png = new PNG({ filterType: 4 })
 
-      png.parse(fileBuffer, (error, data) => {
-        if (error) {
-          reject()
-        }
+        png.parse(fileBuffer, (error: unknown, data: ImageData) => {
+          if (error) {
+            reject()
+          }
 
-        resolve(data)
-      })
+          resolve(data)
+        })
+      } else {
+        reject()
+      }
     })
   }
 }
---
+++ app/Models/ImageData.ts
@@ -0,0 +1,5 @@
+export default interface ImageData {
+  data: Buffer
+  height: number
+  width: number
+}
